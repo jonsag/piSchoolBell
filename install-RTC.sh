ad063b7ec14bc2638b5ba16e29cfbd79fedd75c8
jon <jon@HP-Pavilion-15>
2018-04-18 23:20:14 +0200

    Changed permissions

---
+++ install-RTC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Die on any errors
+set -e
+
+clear
+
+
+if [[ `whoami` != "root" ]]
+then
+  printf "\n\n Script must be run as root. \n\n"
+  exit 1
+fi
+
+
+printf "\n\n Installing real-time-clock ...\n"
+sed -i '/exit 0/i at now + 1 min < /home/pi/bin/piSchoolBell/startRTC.sh' /etc/rc.local
+echo "dtoverlay=i2c-rtc,ds3231" >> /boot/config.txt
+echo "HWCLOCKACCESS=no" >> /etc/default/hwclock
+apt-get purge fake-hwclock -y
+update-rc.d -f fake-hwclock remove
